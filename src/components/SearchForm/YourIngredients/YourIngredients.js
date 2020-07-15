0664ce93c49f37ee7596a64a4b9d1f634a357a0a
FCastillo-91 <fionacastillo@rentalcars.com>
2020-07-15 21:28:40 +0100

    Delete ingredient functionality with filter

--- src/components/SearchForm/YourIngredients/YourIngredients.js
+++ src/components/SearchForm/YourIngredients/YourIngredients.js
@@ -2,13 +2,31 @@ import React from "react";
 import { useAppState } from "../../../context/AppState";
 
 const YourIngredients = () => {
-  const { ingredients } = useAppState();
+  const { ingredients, setIngredients } = useAppState();
+
+  const deleteIngredient = (ingredient) => {
+    const updatedIngredients = ingredients.filter((item) => {
+      return item !== ingredient;
+    });
+    setIngredients(updatedIngredients);
+  };
+
   return (
     <div className="card h-100">
       <div className="card-body">
         <h3 className="card-title">Your ingredients</h3>
         {ingredients.map((ingredient, index) => {
-          return <button data-testid='an-ingredient' key={index}>{ingredient}</button>;
+          return (
+            <button
+              onClick={(e) => {
+                deleteIngredient(ingredient);
+              }}
+              data-testid="an-ingredient"
+              key={index}
+            >
+              {ingredient}
+            </button>
+          );
         })}
       </div>
     </div>
